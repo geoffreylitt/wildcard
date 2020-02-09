3d865e9dcd269f8e91a06e56da4a9dae4fb7b1d1
Geoffrey Litt <gklitt@gmail.com>
2020-02-09 13:28:15 -0500

    add stub instructions

--- README.md
+++ README.md
@@ -34,12 +34,42 @@ To get code changes to automatically update the extension in the browser:
 Every time you develop, follow these steps. If a change you make isn't having an effect, it's probably related to these steps:
 
 * `yarn run dev` to start a watcher that builds the project.
-  * Compilation can take a few seconds. If a change isn't working, it might be that compilation didn't finish yet. (Improving compilation time is a todo; I think it's mostly time spent compiling Handsontable)
+* Compilation can take a few seconds. If a change isn't working, it might be that compilation didn't finish yet. (Improving compilation time is a todo; I think it's mostly time spent compiling Handsontable)
 
 To test if you're able to make changes, try adding a `console.log` statement to a site adapter file like `src/site_adapters/airbnb.ts` and see if it works.
 
 ### File layout
 
-`src/wildcard.ts` contains the core framework.
+`src/core.ts` contains the core framework.
 
 Site adapters are in `src/site_adapters`. Cell editors are in `src/cell_editors`.
+
+`src/wildcard.ts` is the final file that pulls everything together and
+injects Wildcard into the page.
+
+### To add a new site adapter
+
+(Todo: flesh this out in way more detail, point to the auto-generated docs)
+
+Copy `src/site_adapters/airbnb.ts` and mimic the format of that file.
+
+Define your adapter with the following fields:
+
+* a name
+* a url pattern (will match the URL with a simple substring match)
+* getRowContainer: returns the div containing all rows
+* getDataRows: return an array of divs representing rows
+* colSpecs: details about which data to extract from each row
+* setupReloadTriggers: react to data changes and reload the data.
+
+Finally, register your adapter in `src/wildcard.ts`:
+
+```
+import { AirbnbAdapter } from './site_adapters/airbnb';
+
+const siteAdapters = [
+//...
+AirbnbAdapter
+//...
+]
+```
--- rollup.config.js
+++ rollup.config.js
@@ -5,82 +5,23 @@ import postcss from 'rollup-plugin-postcss';
 import typescript from 'rollup-plugin-typescript2';
 
 export default [
-  {
-    input: 'src/wildcard.ts',
-    output: {
-      file: 'dist/wildcard.js',
-      format: 'iife'
-    },
-    plugins: [
-      resolve(),
-      commonjs({
+{
+  input: 'src/wildcard.ts',
+  output: {
+    file: 'dist/wildcard.js',
+    format: 'iife'
+  },
+  plugins: [
+  resolve(),
+  commonjs({
         // non-CommonJS modules will be ignored, but you can also
         // specifically include/exclude files
         include: 'node_modules/**',  // Default: undefined
       }),
-      postcss({
-        extensions: ['.css'],
-      }),
-      typescript()
-    ]
-  },
-  // {
-  //   input: 'src/site_adapters/expedia.ts',
-  //   output: {
-  //     file: 'dist/expedia.js',
-  //     format: 'iife'
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     commonjs({
-  //       // non-CommonJS modules will be ignored, but you can also
-  //       // specifically include/exclude files
-  //       include: 'node_modules/**',  // Default: undefined
-  //     }),
-  //     postcss({
-  //       extensions: ['.css'],
-  //     }),
-  //     typescript()
-  //   ]
-  // },
-  // {
-  //   input: 'src/site_adapters/airbnb.ts',
-  //   output: {
-  //     file: 'dist/airbnb.js',
-  //     format: 'iife'
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     commonjs({
-  //       // non-CommonJS modules will be ignored, but you can also
-  //       // specifically include/exclude files
-  //       include: 'node_modules/**',  // Default: undefined
-  //     }),
-  //     postcss({
-  //       extensions: ['.css'],
-  //     }),
-  //     typescript()
-  //   ]
-  // },
-  // {
-  //   input: 'src/site_adapters/todomvc.ts',
-  //   output: {
-  //     file: 'dist/todomvc.js',
-  //     format: 'iife'
-  //   },
-  //   plugins: [
-  //     resolve(),
-  //     commonjs({
-  //       // non-CommonJS modules will be ignored, but you can also
-  //       // specifically include/exclude files
-  //       include: 'node_modules/**',  // Default: undefined
-  //     }),
-  //     postcss({
-  //       extensions: ['.css'],
-  //     }),
-  //     typescript()
-  //   ]
-  // },
-  // add one rollup output per site for now.
-  // in the future, will need to rethink the deployment model.
+  postcss({
+    extensions: ['.css'],
+  }),
+  typescript()
+  ]
+},
 ];
