6f57b3c304f311e8cdc4f04e80a2641237a0746b
Tyler Millis <tmillis@mit.edu>
2020-02-19 17:36:18 -0500

    Fixed blogger site adapter to fit with new site adapter framework

--- src/site_adapters/blogger.ts
+++ src/site_adapters/blogger.ts
@@ -1,20 +1,19 @@
 import { RichTextEditor } from '../cell_editors/richTextEditor.js'
+import {urlContains} from "../utils";
 
 export const BloggerAdapter = {
     name: "Blogger",
-    urlPattern: "blogger.com",
+    enable: () => urlContains("blogger.com"),
     colSpecs: [
         { name: "id", type: "text", hidden: true },
-        { name: "document", editable: true, renderer: 'html', type: "text", editor: RichTextEditor },
+        { name: "document", editable: true, type: "text", editor: RichTextEditor },
         { name: "source", editable: true, renderer: 'html', type: "text", editor: RichTextEditor },
     ],
     getDataRows: () => {
-        let container = document.getElementById("blogger-app");
-        console.log(container);
-        let container2 : HTMLElement = container;
+        let container : HTMLElement = document.getElementById("blogger-app");
         return [
             {
-                el: container2,
+                els: [container],
                 dataValues: {
                     id: 1, // only one row so we can just hardcode an ID
                     document: container.querySelector("#postingComposeBox"),
