e91c3e3d8eba64c07451070db354c510a7e5cf9a
Jessica Quaye <jquaye@mit.edu>
2020-04-28 02:21:54 +0000

    Fixed 'undefined' bug in UberEats

--- src/site_adapters/ubereats.ts
+++ src/site_adapters/ubereats.ts
@@ -30,44 +30,55 @@ const UberEatsAdapter = createDomScrapingAdapter({
             prefix = el.children[0].children[2];
           }
 
-          let r_name = prefix.children[0].innerText;
+          let restaurant = "N/A";
+          if (!(prefix.children[0] == undefined)){
+            restaurant = prefix.children[0].innerText;
+          }
 
-          let pricey_html = <HTMLElement> prefix.children[1];
-          let pricey_text = pricey_html.innerText;
-          let bullet_idx = pricey_text.indexOf("•");
-          let r_pricey = pricey_text.substring(0, bullet_idx);
-          let r_category = pricey_text.substring(bullet_idx+1,pricey_text.length);
+          let pricey_text = "N/A";
+          let r_category = "N/A";
+          let r_pricey = "N/A";
+          if (!(prefix.children[1] == undefined)){
+            let pricey_html = <HTMLElement> prefix.children[1];
+            pricey_text = pricey_html.innerText;
+            let bullet_idx = pricey_text.indexOf("•");
+            r_pricey = pricey_text.substring(0, bullet_idx);
+            r_category = pricey_text.substring(bullet_idx+1,pricey_text.length);
+          }          
 
-          let delivery_metadata = prefix.children[2].children[0];
+         
           let r_rating = 0;
           let r_fee = 0;
 
-          //there are no ratings for the restaurant
-          if (delivery_metadata.children.length == 3){
+          if (!(prefix.children[2] == undefined || prefix.children[2].children[0] == undefined)){
+              let delivery_metadata = prefix.children[2].children[0];
+              //there are no ratings for the restaurant
+              if (delivery_metadata.children.length == 3){
 
-            let fee_html = <HTMLElement> delivery_metadata.children[2].children[1];
-            let fee_text = fee_html.innerText;
-            let fee_end = fee_text.indexOf("D");
-            r_fee = parseFloat(fee_text.substring(1,fee_end));
-          }
+                let fee_html = <HTMLElement> delivery_metadata.children[2].children[1];
+                let fee_text = fee_html.innerText;
+                let fee_end = fee_text.indexOf("D");
+                r_fee = parseFloat(fee_text.substring(1,fee_end));
+              }
 
-          else if (delivery_metadata.children.length > 3) {
-            let rat_html = <HTMLElement> delivery_metadata.children[2].children[1];
-            let rat_text = rat_html.innerText;
-            let rat_end = rat_text.indexOf("\n");
-            r_rating = parseFloat(rat_text.substring(0,rat_end));
+              else if (delivery_metadata.children.length > 3) {
+                let rat_html = <HTMLElement> delivery_metadata.children[2].children[1];
+                let rat_text = rat_html.innerText;
+                let rat_end = rat_text.indexOf("\n");
+                r_rating = parseFloat(rat_text.substring(0,rat_end));
 
-            let fee_html = <HTMLElement> delivery_metadata.children[4].children[1];
-            let fee_text = fee_html.innerText;
-            let fee_end = fee_text.indexOf("D");
-            r_fee = parseFloat(fee_text.substring(1,fee_end));
+                let fee_html = <HTMLElement> delivery_metadata.children[4].children[1];
+                let fee_text = fee_html.innerText;
+                let fee_end = fee_text.indexOf("D");
+                r_fee = parseFloat(fee_text.substring(1,fee_end));
+              }
           }
 
           return {
             id: el.getAttribute("href"),
             rowElements: [el],
             dataValues: {
-                name: r_name,
+                name: restaurant,
                 notes: r_category,
                 priceyness: r_pricey,
                 rating: r_rating,
