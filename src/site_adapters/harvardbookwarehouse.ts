aac2b62c57524c81ddc881ac30bafbc36ce7a897
Geoffrey Litt <gklitt@gmail.com>
2020-06-21 13:40:13 -0400

    clean up harvard book adapter

--- src/site_adapters/harvardbookwarehouse.ts
+++ src/site_adapters/harvardbookwarehouse.ts
@@ -1,8 +1,6 @@
 'use strict';
 
-// A sample new HN site adapter.
-
-import { urlExact, urlContains, extractNumber } from "../utils";
+import { urlContains } from "../utils";
 import { createDomScrapingAdapter } from "./domScrapingBase"
 
 // Configuration options for the Hacker News adapter
@@ -23,32 +21,13 @@ const HarvardBookWarehouse = createDomScrapingAdapter({
       return {
         id: el.querySelector("a.grid-view-item__link").getAttribute('href'),
         rowElements: [el],
-        // todo: Both of these steps should be handled by the framework...
-        // .filter(e => e) // Only include if the element is really there
-        // .map(e => (e)), // Convert to HTMLElement type
         dataValues: {
           title: el.querySelector("a.grid-view-item__link span.visually-hidden"),
           priceRegular: el.querySelector(".price__regular .price-item--regular"),
           priceSale: el.querySelector(".price__sale .price-item--regular"),
-          soldOut: el.querySelector(".price__badge--sold-out") && window.getComputedStyle(el.querySelector(".price__badge--sold-out")).getPropertyValue('display') !== 'none'
+          soldOut: el.querySelector(".price__badge--sold-out") &&
+            window.getComputedStyle(el.querySelector(".price__badge--sold-out")).getPropertyValue('display') !== 'none'
         },
-        // annotationContainer: el.querySelector("td.subtext") as HTMLElement,
-        // annotationTemplate: `| <span style="color: #f60;">$annotation</span>`
-      }
-    })
-  },
-  onRowSelected: (row) => {
-    row.rowElements.forEach(el => {
-      if (el.style) {
-        el.style["background-color"] = "#def3ff"
-      }
-    });
-    row.rowElements[0].scrollIntoView({ behavior: "smooth", block: "center" })
-  },
-  onRowUnselected: (row) => {
-    row.rowElements.forEach(el => {
-      if (el.style) {
-        el.style["background-color"] = ``
       }
     })
   },
