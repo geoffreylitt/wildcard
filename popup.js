ff82afb319ac0621f4b616ca104415c844cc50bc
Kapaya <kkatongo@mit.edu>
2020-10-02 13:13:51 -0400

    Remove unneeded console.log calls

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
 (function(){
-    console.log(document);
     const generateConfigButton = document.getElementById('generateConfig');
     const selectorsInput = document.getElementById('selectorsInput');
     generateConfigButton.addEventListener('click', (event) => {
       const selectors = selectorsInput.value.split(",").map(selector => selector.trim());
       selectors.reverse();
-      console.log("SELECTORS:", selectors);
       if (selectors && selectors.length) {
         const message = { command: 'generateScraper', selectors };
         chrome.runtime.sendMessage(message, (response) => {
