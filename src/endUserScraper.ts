02edcb63752e15e4df2779ddcbf2a335966f2da6
Kapaya <kkatongo@mit.edu>
2020-10-15 14:37:27 -0400

    Working on prototype of pbd scraper - fix lca bug

--- src/endUserScraper.ts
+++ src/endUserScraper.ts
@@ -1,3 +1,56 @@
+function evaluateXpath(xpath) {
+    return document.evaluate(xpath, document, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+}
+function xpathQuerySelector(xpath) {
+    const elements = [];
+    const iterator = evaluateXpath(xpath);
+    let element = iterator.iterateNext();
+    while (element) {
+        elements.push(element);
+        element = iterator.iterateNext();
+    }
+    return elements;
+}
+function getRowElementXPath(node){
+    let xpath = generateXpath(node);
+    if (xpath) {
+        let score = xpathQuerySelector(xpath).length;
+        let done = false;
+        while (!done) {
+            const matches = xpathQuerySelector(xpath + '/..').length;
+            if (matches < score){
+                done = true;
+            } else {
+                score = matches;
+                xpath += '/..';
+            }
+        }
+        return { 
+            xpath,
+            matches: xpathQuerySelector(xpath)
+        }
+    }
+    return null;
+}
+function xpathBasedListener() {
+    const _matches = new Map();
+    document.body.addEventListener('mousemove', (event) => {
+        const result = getRowElementXPath(event.target);
+        if (result) {
+            const { xpath, matches } = result;
+            _matches.forEach((_style, _match) => {
+                _match.style = _style;
+            });
+            _matches.clear();
+            matches.forEach(match => {
+                if (match.nodeType === Node.ELEMENT_NODE) {
+                    _matches.set(match, match.style)
+                    match.style.border =  "1px solid red";
+                }
+            });
+        }
+    });
+}
 function getAncestors(node) {
     const ancestors = [node];
     let ancestor = node.parentNode;
@@ -8,7 +61,7 @@ function getAncestors(node) {
     return ancestors;
 }
 function containsNode(nodes, node) {
-    const found = nodes.find(_node => _node.isEqualNode(node));
+    const found = nodes.find(_node => _node.isSameNode(node));
     const isParent = nodes.some(_node => node.contains(_node));
     return !!found || !!isParent;
 }
@@ -28,71 +81,164 @@ function findLCA(nodes) {
     }
     if (common.length > 1) {
         return findLCA(common);
+    } else if (common[0]) {
+        return common[0];
+    } else {
+        return nodes[0];
     }
-    return common[0];
 }
-function findRowElement(selectors, lca) {
-    const candidates = [];
-    const point = 1/selectors.length;
-    let candidate = lca;
-    while (candidate && candidate.tagName !== 'BODY') {
-        const candidateEntry = {
-            candidate,
-            weight: 0
-        };
-        let sibling = candidate.nextElementSibling;
-        while (sibling) {
-            selectors.forEach(selector => {
-                if (sibling.querySelector(selector)) {
-                    candidateEntry.weight += point;
-                }
-            });
-            sibling = sibling.nextElementSibling;
-        }
-        candidates.push(candidateEntry);
-        candidate = candidate.parentNode;
-    }
-    candidates.sort((a, b) => {
-        if (a.weight > b.weight) {
-            return -1
-        } else if (a.weight < b.weight) {
-            return 1
-        } else {
-            return 0;
-        }
-    });
-    return candidates[0].candidate;
-}
-function generateNodeSelector(node){
+function generateNodeSelector(node) {
     let selector = node.tagName.toLowerCase();
-    if (node.id) {
-        selector += `#${node.id}`;
-    } else if (node.classList && node.classList.length) {
+    if (node.classList && node.classList.length) {
         selector += `.${Array.from(node.classList).join('.')}`;
     }
     return selector;
 }
-function generateRowElementSelector(rowElement) {
-    const selectorArray = [];
-    let _node = rowElement;
-    let selector;
-    while(_node && _node.tagName !== "BODY") {
-        selector = generateNodeSelector(_node)
-        selectorArray.unshift(selector);
-        _node = _node.parentNode;
-        if (/[#]+/.test(selector)) {
-            break;
-        }
-    }
-    return selectorArray.join(">");
+function generateIndexedNodeSelector(node) {
+    const tag = node.tagName.toLowerCase();
+    const index = Array.prototype.indexOf.call(node.parentNode.children, node) + 1;
+    return `${tag}:nth-child(${index})`;
 }
-export function generateScraper(selectors) {
-    const nodes = selectors.map(selector => document.querySelector(selector));
-    const lca = findLCA(nodes);
-    const rowElement = findRowElement(selectors, lca);
-    const rowElementSelector = generateRowElementSelector(rowElement);
-    const name = document.title;
-    const attributes = selectors.map((_, index) => {
+function generateNodeSelectorFromBody(node) {
+    const selectors = [];
+    let _node = node;
+    while (_node && _node.tagName !== "BODY") {
+        selectors.unshift(generateNodeSelector(_node));
+        _node = _node.parentNode;
+    }
+    return selectors.join(">");
+}
+function generateIndexedNodeSelectorFrom(node, from) {
+    const selectors = [];
+    let _node = node;
+    while (!_node.isSameNode(from)) {
+        selectors.unshift(generateIndexedNodeSelector(_node));
+        _node = _node.parentNode;
+    }
+    return selectors.join('>');
+}
+function findRowElement(nodes, lca) {
+    const candidates = [];
+    let selectors = nodes.map(node => generateIndexedNodeSelectorFrom(node, lca));
+    let candidate = lca;
+    while (candidate && candidate.tagName !== 'BODY') {
+        const candidateEntry = {
+            candidate: candidate,
+            score: 0,
+            targetNodeSelectors: selectors
+        };
+        let nextSibling = candidate.nextElementSibling;
+        let previousSibling = candidate.previousElementSibling;
+        while (nextSibling) {
+            selectors.forEach(selector => {
+                if (nextSibling.querySelector(selector)) {
+                    candidateEntry.score += 1;
+                }
+            });
+            nextSibling = nextSibling.nextElementSibling;
+        }
+        while (previousSibling) {
+            selectors.forEach(selector => {
+                if (previousSibling.querySelector(selector)) {
+                    candidateEntry.score += 1;
+                }
+            });
+            previousSibling = previousSibling.previousElementSibling;
+        }
+        candidates.push(candidateEntry);
+        candidate = candidate.parentNode;
+        selectors = selectors.map(selector => `${generateIndexedNodeSelector(candidate)}>${selector}`);
+    }
+    if (candidates.length) {
+      candidates.sort(function (a, b) {
+          if (a.score > b.score) {
+              return -1;
+          }
+          else if (a.score < b.score) {
+              return 1;
+          }
+          else {
+              return 0;
+          }
+      });
+      return {
+          rowElement: candidates[0].candidate,
+          rowElementSelector: generateNodeSelectorFromBody(candidates[0].candidate),
+          targetNodeSelectors: candidates[0].targetNodeSelectors
+      };
+    }
+    return null
+}
+ 
+function generateXpath(node) {
+    const xpath = [];
+    let _node = node;
+    let selector;
+    while (_node && _node.tagName && _node.tagName !== 'BODY') {
+        selector = _node.tagName;
+        if (_node.classList && _node.classList.length) {
+            selector += `[contains(@class, '${Array.from(_node.classList).join(' ')}')]`;
+        }
+        xpath.push(selector);
+        _node = _node.parentNode;
+    }
+    if (xpath.length) {
+        xpath.reverse();
+        xpath[0] = `//BODY/${xpath[0]}`;
+        return xpath.join('/');
+    }
+    return null;
+}
+function cssBasedListner() {
+    const _matches = new Map();
+    const targetNodes = [];
+    document.body.addEventListener('mousedown', (event) => {
+        _matches.forEach((_style, _match) => {
+            _match.style = _style;
+        });
+        _matches.clear();
+        const target = event.target as HTMLElement;
+        if (targetNodes.indexOf(target) === -1) {
+            targetNodes.push(target);
+            let lca = findLCA(targetNodes);
+            if (lca.isSameNode(targetNodes[0])) {
+                lca = lca.parentNode;
+            }
+            const result = findRowElement(targetNodes, lca);
+            if (result) {
+                const { rowElementSelector, targetNodeSelectors } = result;
+                Array.from(document.querySelectorAll(rowElementSelector))
+                .forEach((rowElement) => {
+                    targetNodeSelectors.forEach(selector => {
+                        const match = rowElement.querySelector(selector);
+                        if (match && match.nodeType === Node.ELEMENT_NODE) {
+                            _matches.set(match, match.style)
+                            match.style.border =  "1px solid red";
+                        }
+                    });
+                });
+                const { config } = generateScraper(targetNodeSelectors, rowElementSelector);
+                console.log(config);
+            }
+        }
+    });
+    document.body.addEventListener('mousemove', (event) => {
+        const target = event.target as HTMLElement;
+        _matches.forEach((_style, _match) => {
+           _match.style.backgroundColor = _style;
+        });
+        _matches.clear();
+        _matches.set(target, target.style.backgroundColor)
+        target.style.backgroundColor =  "red";
+    });
+}
+
+export function startScraper() {
+  cssBasedListner();
+}
+
+export function generateScraper(targetSelectors, rowElementSelector) {
+    const attributes = targetSelectors.map((_, index) => {
         return {
             name: String(index),
             type: "text"
@@ -106,7 +252,7 @@ export function generateScraper(selectors) {
         scrapePage: () => {
             return Array.from(document.querySelectorAll("${rowElementSelector}")).map((element, index) => {
                 const dataValues = {};
-                ${JSON.stringify(selectors)}.forEach((selector, index) => {
+                ${JSON.stringify(targetSelectors)}.forEach((selector, index) => {
                     const selected = element.querySelector(selector);
                     dataValues[index] = selected? selected.textContent : "";
                 });
